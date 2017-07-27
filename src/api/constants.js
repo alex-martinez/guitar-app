8f1f0ee96ccc9b7d1fd9a5a3ef4bfc12cd3f2a7f
Alex <amartinez@getsmartcontent.com>
2017-07-26 22:12:04 -0500

    Add more scales

--- src/api/constants.js
+++ src/api/constants.js
@@ -22,14 +22,19 @@ export const NOTE_MAP = {
 };
 
 export const SCALE = {
-  major: [0, 2, 4, 5, 7, 9, 11], // w w h w w w (Ionian)
-  minor: [0, 2, 3, 5, 7, 8, 10], // w h w w h w (Aeolian)
-  natural_minor: [0, 2, 3, 5, 7, 8, 10],
+  major: [0, 2, 4, 5, 7, 9, 11], // Ionian
+  major_pentatonic: [0, 2, 4, 7, 9],
+  natural_minor: [0, 2, 3, 5, 7, 8, 10], // Aeolian
+  minor_pentatonic: [0, 3, 5, 7, 10],
+  harmonic_minor: [0, 2, 3, 5, 7, 8, 11],
+  blues_pentatonic: [0, 3, 5, 6, 7, 10],
+  mixolydian: [0, 2, 4, 5, 7, 9, 10],
+  dorian: [0, 2, 3, 5, 7, 9, 10],
 };
 
 export const CHORD = {
   major: [0, 4, 7],
-  major7: [0, 4, 7, 11],
+  major_7: [0, 4, 7, 11],
   minor: [0, 3, 7],
-  minor7: [0, 3, 7, 10],
+  minor_7: [0, 3, 7, 10],
 };
